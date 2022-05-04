0f921420781f59a8f52389e7f8c1549d8432c1a7
Divyansh Singh <sdivyansh59@gmail.com>
2022-05-04 16:00:39 +0530

    It's done , now I have to work on make it live

--- spring-crudApp/crudapp/src/main/java/com/empmanagement/crudapp/controller/EmployeeController.java
+++ spring-crudApp/crudapp/src/main/java/com/empmanagement/crudapp/controller/EmployeeController.java
@@ -3,8 +3,15 @@ package com.empmanagement.crudapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,8 +34,30 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		List<Employee> temp =  eRepo.findAll();
-		System.out.println(temp);
+//		System.out.println(temp);
 		return temp;
 	}
+	
+	@PostMapping("/employees")
+	public Employee saveEmployeeDetails( @RequestBody Employee employee) {
+		return eRepo.save(employee);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public Employee getSingleEmployee(@PathVariable Long id) {
+		return eRepo.findById(id).get();
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployeeDetails(@RequestBody Employee employee) {
+		return eRepo.save(employee);
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<HttpStatus> deleteSingleEmployee(@PathVariable Long id) {
+		 eRepo.deleteById(id); // it return void
+		 return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+	}
+	
 
 }
